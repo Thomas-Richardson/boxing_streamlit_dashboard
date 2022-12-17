5860273506edd873077521b5ca0e850b272d0411
Thomas-Richardson <thomas.m.richardson@protonmail.com>
2022-12-17 14:36:39 +0000

    Made cleaning script

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 import streamlit as st
 import pandas as pd
+import seaborn as sns 
+import matplotlib.pyplot as plt
 
 
+# Server
 df= pd.read_csv("boxing_data.csv")
 
-st.write("Hello world")
-
+# UI
 st.dataframe(df)
